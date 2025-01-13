4f56d2b60c67f5fe8355d453622d22982e1fd516
Ajay-kannan2006 <ajaykannan1001200635@gmail.com>
2025-01-13 13:37:40 +0530

    home Page design changed and made responsive

--- src/Home/LeftHome.jsx
+++ src/Home/LeftHome.jsx
@@ -1,9 +1,11 @@
 import './Home.css'
-
+import profileImg from '../assets/Profile.jpg';
 export default function LeftHome() {
     return (
         <div id="left-home">
-            <h1>Hi I'm<span>Full Stack Developer</span></h1>
+            <div class="profile-picture">
+                <img src={profileImg} alt="Your Picture" />
+            </div>
         </div>
     );
 }
--- src/Home/RightHome.jsx
+++ src/Home/RightHome.jsx
@@ -1,10 +1,20 @@
 import './Home.css'
-import profileImg from '../assets/Profile.jpg';
+import { SiLeetcode } from "react-icons/si";
 export default function RightHome() {
     return (
-        <div id="right-home">
-            <div class="profile-picture">
-                <img src={profileImg} alt="Your Picture" />
+        <div id='right-home'>
+            <div className="home-content">
+                <h1>Hi, I'm <span class="highlight">Ajay K K</span></h1>
+                <h2>A Passionate <span class="highlight">Full Stack Developer</span></h2>
+                <p>
+                    I specialize in building responsive, scalable, and efficient web applications.
+                    Let's create something amazing together!
+                </p>
+                <div class="home-buttons">
+                    <a href="" className="btn-icons"><i class="fa-brands fa-github fa-xl"></i></a>
+                    <a href="#projects" className="btn-icons"><i class="fa-brands fa-linkedin fa-xl"></i></a>
+                    <a href="" className="btn-icons leet"><SiLeetcode /></a>
+                </div>
             </div>
         </div>
     );
